aaac26a5750ecaa8bbab87af66d86d95cb6610a0
Vlad Kornyakov <time2begin666@gmail.com>
2024-04-19 10:03:59 +0300

    Lesson 217

--- gulpfile.js
+++ gulpfile.js
@@ -23,22 +23,22 @@ gulp.task("build-js", () => {
                     devtool: "source-map",
                     module: {
                         rules: [
-                          {
-                            test: /\.m?js$/,
-                            exclude: /(node_modules|bower_components)/,
-                            use: {
-                              loader: 'babel-loader',
-                              options: {
-                                presets: [['@babel/preset-env', {
-                                    debug: true,
-                                    corejs: 3,
-                                    useBuiltIns: "usage"
-                                }]]
-                              }
-                            }
-                          }
+							{
+								test: /\.m?js$/,
+								exclude: /(node_modules|bower_components)/,
+								use: {
+									loader: 'babel-loader',
+									options: {
+										presets: [['@babel/preset-env', {
+											debug: true,
+											corejs: 3,
+											useBuiltIns: "usage"
+										}]]
+									}
+								}
+							}
                         ]
-                      }
+                    }
                 }))
                 .pipe(gulp.dest(dist))
                 .on("end", browsersync.reload);
@@ -73,21 +73,21 @@ gulp.task("build-prod-js", () => {
                     },
                     module: {
                         rules: [
-                          {
-                            test: /\.m?js$/,
-                            exclude: /(node_modules|bower_components)/,
-                            use: {
-                              loader: 'babel-loader',
-                              options: {
-                                presets: [['@babel/preset-env', {
-                                    corejs: 3,
-                                    useBuiltIns: "usage"
-                                }]]
-                              }
-                            }
-                          }
+							{
+								test: /\.m?js$/,
+								exclude: /(node_modules|bower_components)/,
+								use: {
+									loader: 'babel-loader',
+									options: {
+										presets: [['@babel/preset-env', {
+											corejs: 3,
+											useBuiltIns: "usage"
+										}]]
+									}
+								}
+							}
                         ]
-                      }
+                    }
                 }))
                 .pipe(gulp.dest(dist));
 });
